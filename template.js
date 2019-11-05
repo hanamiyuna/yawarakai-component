db10e99f0edd62d279b17366fa2fafdc9170e4fe
Hanami Yuna <neko@ayaka.moe>
2019-11-05 20:01:45 +0800

    Added the Template for basic component

---
+++ template.js
@@ -0,0 +1,63 @@
+// Dependecies
+
+let Compo = require('../../component')
+
+// Component Method
+
+let config = require('./config.json')
+
+// Change the Component Name according to the config.json
+
+exports.meta = config.components.componentName
+
+// Inner
+
+exports.commands = {
+    main: async function () {
+
+    }
+}
+
+exports.inlines = {
+    main: async function () {
+
+    }
+}
+
+exports.message = {
+    main: async function () {
+
+    }
+}
+
+exports.callbackQuery = {
+    main: async function () {
+
+    }
+}
+
+// Register
+
+exports.register = {
+    // As the example to Yawarakai Compos
+    commands: [
+        // {
+            // function: 'main'
+        // }
+    ],
+    inlines: [
+        // {
+            // function: "main"
+        // }
+    ],
+    messages: [
+        // {
+            // function: 'main'
+        // }
+    ],
+    callbackQuery: [
+        // {
+        //     function: 'main'
+        // }
+    ]
+}
